e5b7df482eda6fedf2eb7a11a63fa8e874bb1a0a
pyzen5 <pyzen55@gmail.com>
2020-05-06 16:28:49 +0530

    high level changes

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -9,16 +9,16 @@ export class EmployeesComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   employeeList = [
     // tslint:disable-next-line: max-line-length
-    { empId: 1001, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore' },
+    { empId: 1001, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore', department: 'Front End Development', rollType: 'Full Time' },
     // tslint:disable-next-line: max-line-length
-    { empId: 1002, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore' },
+    { empId: 1002, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore', department: 'Front End Development', rollType: 'Full Time' },
     // tslint:disable-next-line: max-line-length
-    { empId: 1003, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore' },
+    { empId: 1003, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore', department: 'Front End Development', rollType: 'Full Time' },
     // tslint:disable-next-line: max-line-length
-    { empId: 1004, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2018', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore' },
+    { empId: 1004, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2018', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore', department: 'Front End Development', rollType: 'Full Time' },
     // tslint:disable-next-line: max-line-length
-    { empId: 1005, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore' },
-    { empId: 1005, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Mumbai' }
+    { empId: 1005, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Bangalore', department: 'Front End Development', rollType: 'Full Time' },
+    { empId: 1005, avatarPath: '', name: 'Andrew Bridgen', designation: 'Sr. UI Developer', rating: 3.5, experience: '5.8 years', doj: '2017', currentTeam: 'OCBC Singapore', rm: 'Lalit Agarwal', mobile: '7406559241', email: 'andrew@infrrd.ai', location: 'Mumbai', department: 'Front End Development', rollType: 'Full Time' }
   ];
   filteredList = [];
 
